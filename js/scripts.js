1a6b92fb8d411b8a4c22b0775eb721d7d3e35dc2
Jon Avila <jon@zombierooster.com>
2018-09-20 17:03:10 -0400

    worked on select buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,25 @@ $('#name').focus();
 $('#other-title').hide();
 $('#title').on('change', function() {
     const value = $(this).val();
-    console.log(value);
     if(value === 'other'){
         $('#other-title').show();
     } else {
         $('#other-title').hide();
+    }
+});
+$('#design').on('change', function() {
+    const value = $(this).val();
+    console.log(value);
+    if(value === 'js puns'){
+        $("#color option:contains('(I')").hide();
+        $("#color option:contains('Puns')").show();
+        $("#color option:contains('Puns')").first().attr('selected','selected');
+        $("#color option:contains('(I')").first().attr('selected',false);
+
+    } else if(value ==='heart js'){
+        $("#color option:contains('Puns')").hide();
+        $("#color option:contains('(I')").show();
+        $("#color option:contains('(I')").first().attr('selected','selected');
+        $("#color option:contains('Puns')").first().attr('selected',false);
     }
 });
